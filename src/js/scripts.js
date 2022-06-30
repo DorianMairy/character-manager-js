afb98ae950f23954bd71299f52b8900ee0a10f33
root <root@pop-os.localdomain>
2022-06-30 13:29:19 +0200

    css

---
+++ src/js/scripts.js
@@ -0,0 +1,10 @@
+const containerCharacter = document.querySelector(".container_character");
+
+const buttonReturn = document.querySelector(".return");
+const buttonAdd = document.querySelector(".add_character_button");
+const buttonSubmit = document.querySelector(".submit");
+
+const form = document.querySelector(".add_character");
+
+const inputName = document.querySelector("#nameCharacter");
+const inputDescr = document.querySelector("#description");
