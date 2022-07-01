097c849de06b96e5c8f2814aa96bdb42b77432b9
root <root@pop-os.localdomain>
2022-07-01 16:24:13 +0200

    v2

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,13 +7,14 @@ const btndelete = document.querySelector("btn-delete");
 let i = 0;
 // fonction de chargement des informations des characters
 async function displayCharactersCards() {
-    const response = await fetch(
-        "https://character-database.becode.xyz/characters"
-        );
-        const character = await response.json();
-  console.log(character);
-  console.log(character.length);
-for (i ; i < character.length; i++) {
+  const response = await fetch(
+    "https://character-database.becode.xyz/characters"
+    );
+    const character = await response.json();
+    console.log(character);
+    console.log(character.length);
+    for (i; i < character.length; i++) {
+      const desc = character[i].description;
     // Nom des characters
     let cardS = document.createElement("div");
     cardS.setAttribute("class", "cards");
@@ -33,11 +34,22 @@ for (i ; i < character.length; i++) {
     cardShortDesc.setAttribute("class", "card-short-desc");
     cardS.appendChild(cardShortDesc);
     cardShortDesc.innerHTML = character[i].shortDescription;
-    // Description des characters
-let cardDesc = document.createElement("div");
-cardDesc.setAttribute("class", "card-desc");
-cardS.appendChild(cardDesc);
-cardDesc.innerHTML = character[i].description;
-}
-}
+    // clique sur la photo pour afficher les info
+    cardS.addEventListener("click", function() {
+      cardShortDesc.innerHTML = desc;
+      cardShortDesc.setAttribute("class", "card-short-desc");
+      cardS.appendChild(cardShortDesc);
+      }, false);
+    }
+    // bouton new character
+    let cardBtn = document.createElement("div");
+    cardBtn.setAttribute("class", "btn-add");
+    body.appendChild(cardBtn);
+    cardBtn.innerHTML = "New character";
+    // bouton delete character
+    let cardBtnDel = document.createElement("div");
+    cardBtnDel.setAttribute("class", "btn-delete");
+    body.appendChild(cardBtnDel);
+    cardBtnDel.innerHTML = "Delete character";
+    }
 displayCharactersCards();
